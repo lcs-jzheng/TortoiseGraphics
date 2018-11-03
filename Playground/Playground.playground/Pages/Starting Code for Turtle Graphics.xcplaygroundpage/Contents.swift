6068262f0023b157e7e1229527a22782061ae25c
Jeff Zheng <jzheng@lcs.on.ca>
2018-11-03 10:33:17 -0400

    Starting template

---
+++ Playground/Playground.playground/Pages/Starting Code for Turtle Graphics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+//: # With a tortoise 🐢
+//: [👉 With 2 tortoises 🐢🐢](@next)
+import PlaygroundSupport
+import TortoiseGraphics
+import CoreGraphics
+
+let canvas = PlaygroundCanvas(frame: CGRect(x: 0, y: 0, width: 500, height: 500))
+canvas.frameRate = 30
+canvas.color = .white
+PlaygroundPage.current.liveView = canvas
+
+canvas.drawing { turtle in
+    
+}
