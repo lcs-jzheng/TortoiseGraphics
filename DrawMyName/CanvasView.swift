fa230b6d11502b3460d5ae7b29ad3e1160008eb8
Jeff Zheng <jzheng@lcs.on.ca>
2018-11-04 10:11:09 -0500

    The advanced version of the DrawMyName Project, with Comments

--- DrawMyName/CanvasView.swift
+++ DrawMyName/CanvasView.swift
@@ -28,14 +28,19 @@ class CanvasView: NSView {
             
             turtle.ycor
             turtle.heading
-            
+                //loop up
             turtle.left(45)
             turtle.curve(withSides: 30, withSize: 15, drawSides: 6)
+                //turn around
             turtle.curve(withSides: 30, withSize: 2, drawSides: 14)
+                //go down
             turtle.curve(withSides: 200000, withSize: 18, drawSides: 7)
+                //turn around
             turtle.curve(withSides: 30, withSize: 3, drawSides: 17)
             turtle.curve(withSides: 30, withSize: 2, drawSides: 2)
+                //go striaght
             turtle.curve(withSides: 300000, withSize: 13, drawSides: 5)
+           
             turtle.right(25)
             turtle.right(2)
             
@@ -86,19 +91,18 @@ class CanvasView: NSView {
             turtle.right(20)
             
             //upperCase Z
+                //curve for the upper part of the Z
             turtle.curve(withSides: 40, withSize: 5, drawSides: 5)
             turtle.curve(withSides: 30, withSize: 3, drawSides: 10)
             turtle.curve(withSides: -4000, withSize: 6, drawSides: 1)
             turtle.curve(withSides: 40, withSize: 7, drawSides: 3)
             turtle.curve(withSides: -4000, withSize: 6, drawSides: 5)
-            
+                //curve for the lower part of the Z
             turtle.right(180)
-            
             turtle.curve(withSides: 30, withSize: 5, drawSides: 9)
-            
             turtle.right(45)
-            
             turtle.curve(withSides: 40, withSize: 18, drawSides: 4)
+                //loop for connection
             turtle.curve(withSides: 30, withSize: 2, drawSides: 14)
             turtle.curve(withSides: 30, withSize: 15, drawSides: 4)
             
@@ -111,15 +115,22 @@ class CanvasView: NSView {
             turtle.left(75)
             
             //lowerCase h
+                //loop up
             turtle.curve(withSides: 300000, withSize: 8, drawSides: 8)
+                //turn around
             turtle.curve(withSides: -30, withSize: 2, drawSides: 8)
             turtle.curve(withSides: -40, withSize: 1, drawSides: 11)
+                //going down
             turtle.curve(withSides: 300000, withSize: 15, drawSides: 5)
+                //go up
             turtle.right(180)
             turtle.curve(withSides: 300000, withSize: 15, drawSides: 2)
+                //round for the right part of h
             turtle.curve(withSides: 40, withSize: 1, drawSides: 13)
             turtle.curve(withSides: 30, withSize: 2, drawSides: 5)
+                //going down
             turtle.curve(withSides: 300000, withSize: 9, drawSides: 2)
+                //curve for the end part of the h
             turtle.curve(withSides: -10, withSize: 3, drawSides: 4)
             
             turtle.ycor
@@ -134,28 +145,42 @@ class CanvasView: NSView {
             
             turtle.left(30)
             //lowerCase n
+                //going up for ceonnection
             turtle.curve(withSides: 300000, withSize: 3, drawSides: 11)
+                //turn
             turtle.curve(withSides: 20, withSize: 1, drawSides: 10)
+                //going down for the left part of the n
             turtle.curve(withSides: 300000, withSize: 3, drawSides: 13)
+                //going up
             turtle.right(180)
             turtle.curve(withSides: 300000, withSize: 3, drawSides: 10)
+                //curve for the right part of the n
             turtle.curve(withSides: 10, withSize: 5, drawSides: 5)
+                //going down for the right part of n
             turtle.curve(withSides: 300000, withSize: 3, drawSides: 7)
             turtle.curve(withSides: -10, withSize: 6, drawSides: 5)
             
             turtle.right(12)
             
             //lowerCase g
+                //connective line for g
             turtle.curve(withSides: 300, withSize: 3, drawSides: 10)
+                //turning
             turtle.curve(withSides: 10, withSize: 5, drawSides: 3)
             turtle.right(40)
+                //straight line for g
             turtle.curve(withSides: 350, withSize: 3, drawSides: 25)
             turtle.right(180)
+                //going back up
             turtle.curve(withSides: -350, withSize: 3, drawSides: 21)
             turtle.right(180)
+                //circular part for the g
             turtle.curve(withSides: 35, withSize: 3, drawSides: 35)
+                //go back down
             turtle.curve(withSides: 350, withSize: 3, drawSides: 21)
+                //turn for the last curve
             turtle.curve(withSides: 50, withSize: 1, drawSides: 25)
+                //loop up
              turtle.curve(withSides: 300, withSize: 3, drawSides: 20)
         }
     }
--- Sources/Extension.swift
+++ Sources/Extension.swift
@@ -38,22 +38,33 @@ public extension Tortoise {
     }
 
     func lowerCase_f() {
+            //connective part of the f
         self.curve(withSides: 20000, withSize: 38, drawSides: 2)
+            //turn
         self.curve(withSides: -30, withSize: 2, drawSides: 16)
+            //going down
         self.curve(withSides: -4000, withSize: 12, drawSides: 10)
+            //turn for the bottom part
         self.curve(withSides: -30, withSize: 2, drawSides: 17)
+            //go up
         self.curve(withSides: -30, withSize: 35, drawSides: 1)
         self.right(95)
+            //the small line for the connect of the second letter
         self.curve(withSides: -4000, withSize: 12, drawSides: 1)
 
     }
 
     func lowerCase_e() {
+            //connective line for e
         self.curve(withSides: 200, withSize: 2, drawSides: 3)
+            //little turn
         self.curve(withSides: -30, withSize: 2, drawSides: 6)
         self.right(20)
+            //loop up
         self.curve(withSides: -80, withSize: 3, drawSides: 10)
+            //turn for the upper part of e
         self.curve(withSides: -30, withSize: 1, drawSides: 10)
+            //loop down
         self.curve(withSides: -45, withSize: 3, drawSides: 25)
     }
 }
